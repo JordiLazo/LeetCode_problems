737773a8d52c0bf6488f2402e7a240a7902bf448
Jordi Lazo <jordilazo94@gmail.com>
2021-07-16 14:48:09 +0200

    activity1

---
+++ invert_binary_tree.py
@@ -0,0 +1,84 @@
+'''
+This problem was recently asked by Twitter:
+
+You are given the root of a binary tree. Invert the binary tree in place. That is, all left children should become right children, and all right children should become left children.
+
+Example:
+
+    a
+   / \
+  b   c
+ / \  /
+d   e f
+
+The inverted version of this tree is as follows:
+
+  a
+ / \
+ c  b
+ \  / \
+  f e  d
+'''
+
+# A class to store a binary tree node
+class Node:
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+# Function to perform preorder traversal on a given binary tree
+def preorder(root):
+    if root is None:
+        return
+
+    print(root.data, end=' ')
+    preorder(root.left)
+    preorder(root.right)
+
+
+# Utility function to swap left subtree with right subtree
+def swap(root):
+    if root is None:
+        return
+
+    temp = root.left
+    root.left = root.right
+    root.right = temp
+
+
+# Function to invert a given binary tree using preorder traversal
+def invertBinaryTree(root):
+    # base case: if the tree is empty
+    if root is None:
+        return
+
+    # swap left subtree with right subtree
+    swap(root)
+
+    # invert left subtree
+    invertBinaryTree(root.left)
+
+    # invert right subtree
+    invertBinaryTree(root.right)
+
+if __name__ == '__main__':
+    ''' Construct the following tree
+              1
+            /   \
+           /     \
+          2       3
+         / \     / \
+        4   5   6   7
+    '''
+
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+    root.right.left = Node(6)
+    root.right.right = Node(7)
+
+    invertBinaryTree(root)
+    preorder(root)
